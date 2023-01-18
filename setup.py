d54045a7f683c4ffdab48e539c197fc50d8dc15d
Michele Cotrufo <michele.cotrufo@gmail.com>
2023-01-18 11:27:15 -0500

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements_strict.txt") as f:
     required_packages = f.read().splitlines()
 
 setuptools.setup(name='pyThorlabsAPT',
-      version='0.10',
+      version='0.11',
       description='A python library/GUI to access and control motors via the APT protocol.',
       long_description=long_description,
       long_description_content_type='text/markdown',

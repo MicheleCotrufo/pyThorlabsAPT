479ba4a45c59da6ec291395724c81a2c2295141c
Michele <michele.cotrufo@gmail.com>
2026-06-23 15:11:29 -0400

    fixed dll issue

--- pyThorlabsAPT/thorlabs_apt/__init__.py
+++ pyThorlabsAPT/thorlabs_apt/__init__.py
@@ -6,15 +6,17 @@ def list_available_devices():
     """
     Lists all devices currently connected to the computer.
 
-    This re-initializes the underlying APT library connection before querying for devices (overriding
-    core.list_available_devices(), which does not do this), so that devices plugged in after this
-    package was imported -- or since the last call to this function -- can be discovered.
+    Initializes the underlying APT library on the first call (if not already loaded), then queries
+    for connected devices. The library is intentionally initialized only once: re-initializing it
+    (APTCleanUp + APTInit) would drop all active device connections, breaking any Motor objects that
+    are currently in use. As a consequence, devices plugged in after the first call to this function
+    will not be discovered without restarting the process.
 
     Returns
     -------
     out : list
         list of available devices. Each device is described by a tuple (hardware type, serial number)
     """
-    _core._cleanup()
-    _core._lib = _core._load_library()
+    if _core._lib is None:
+        _core._lib = _core._load_library()
     return _core.list_available_devices()
